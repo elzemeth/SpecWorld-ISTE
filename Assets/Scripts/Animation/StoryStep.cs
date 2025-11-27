c4f0b4fbf5393baa3d69b6c249277091500228a3
elzemeth <hakaneerbas@gmail.com>
2025-11-27 21:25:02 +0300

    Fixed Animations Timeline

--- Assets/Scripts/Animation/StoryStep.cs
+++ Assets/Scripts/Animation/StoryStep.cs
@@ -1,21 +1,24 @@
+using System;
 using UnityEngine;
 
-[System.Serializable]
+[Serializable]
 public class StoryStep
 {
+    [Header("Genel")]
     public string stepName;
-
-    public StoryActionType actionType = StoryActionType.MoveTo;
+    public StoryActionType actionType;
 
     [Header("MoveTo")]
     public Transform moveTarget;
 
     [Header("Talk")]
     public AudioClip voiceClip;
+    public float talkExtraDelay = 0.2f;
 
-    [Header("Delay")]
-    public float waitDuration = 0f;
+    [Header("Wait")]
+    public float waitDuration = 1f;
 
     [Header("PlayAnimation")]
     public AnimationStateId animationId = AnimationStateId.Idle;
+    public float animationDuration = 1.5f;
 }
